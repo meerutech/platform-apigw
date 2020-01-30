f5b3e8e432837f4fbfea9b565c44052be1d9edc3
adam <adam@meerutech.com>
2020-01-30 22:11:24 +0000

    Updates to codepipeline, now complete. Modifying buildpsec to match python version on defined image in codebuild automation.

--- codepipeline/app.py
+++ codepipeline/app.py
@@ -5,23 +5,68 @@ from aws_cdk import (
     aws_codepipeline_actions as codepipeline_actions,
     aws_secretsmanager as secretsmgr, 
     aws_codebuild as codebuild,
+    aws_iam as iam,
     core
 )
 
 from os import getenv
 
 
+class CodeBuildProjects(core.Construct):
+
+    def __init__(self, scope: core.Construct, id: str, buildspec, **kwargs):
+        super().__init__(scope, id, **kwargs)
+        self.buildspec = buildspec
+        self.build_image = codebuild.LinuxBuildImage.STANDARD_2_0
+        
+        self.project = codebuild.PipelineProject(
+            self, "Project",
+            environment=codebuild.BuildEnvironment(
+                build_image=self.build_image
+            ),
+            build_spec=codebuild.BuildSpec.from_source_filename(self.buildspec),
+        )
+        
+        # TODO: Don't need admin, let's make this least privilege
+        self.admin_policy = iam.Policy(
+            self, "AdminPolicy",
+            roles=[self.project.role],
+            statements=[
+                iam.PolicyStatement(
+                    actions=['*'],
+                    resources=['*'],
+                )
+            ]
+        )
+        
+
 class APIGatewayPipeline(core.Stack):
 
     def __init__(self, scope: core.Stack, id: str, **kwargs):
         super().__init__(scope, id, **kwargs)
 
+        # Codebuild projects to use for pipeline
+        # TODO: CDK the codebuild projects
+        #self.codebuild_validate = codebuild.Project.from_project_arn(
+        #    self, "ValidateCodeBuildProject",
+        #    project_arn=getenv('CODEBUILD_VALIDATE_ARN')
+        #    )
+        #    
+        #self.codebuild_deploy = codebuild.Project.from_project_arn(
+        #    self, "BuildCodeBuildProject",
+        #    project_arn=getenv('CODEBUILD_BUILD_ARN')
+        #    )
+        
         # create a pipeline
         self.pipeline = codepipeline.Pipeline(self, "Pipeline", pipeline_name='API_Gateway')
         
         # add a source stage
         self.source_stage = self.pipeline.add_stage(stage_name="Source")
         self.source_artifact = codepipeline.Artifact()
+        
+        # codebuild projects
+        self.codebuild_validate = CodeBuildProjects(self, "CodebuildValidate", buildspec='buildspec-diff.yml')
+        self.codebuild_deploy = CodeBuildProjects(self, "CodebuildDeploy", buildspec='buildspec.yml')
         
         # add source action
         self.source_stage.add_action(codepipeline_actions.GitHubSourceAction(
@@ -33,25 +78,13 @@ class APIGatewayPipeline(core.Stack):
             run_order=1,
         ))
         
-        # Codebuild projects to use for pipeline
-        # TODO: CDK the codebuild projects
-        self.codebuild_validate = codebuild.Project.from_project_arn(
-            self, "ValidateCodeBuildProject",
-            project_arn=getenv('CODEBUILD_VALIDATE_ARN')
-            )
-            
-        self.codebuild_deploy = codebuild.Project.from_project_arn(
-            self, "BuildCodeBuildProject",
-            project_arn=getenv('CODEBUILD_BUILD_ARN')
-            )
-        
         # add validate stage
         self.validate_stage = self.pipeline.add_stage(stage_name='Validate')
         
         # add validate codebuild action
         self.validate_stage.add_action(codepipeline_actions.CodeBuildAction(
             input=self.source_artifact,
-            project=self.codebuild_validate,
+            project=self.codebuild_validate.project,
             action_name='Validate_Changes'
         ))
         
@@ -69,10 +102,10 @@ class APIGatewayPipeline(core.Stack):
         # add deploy codebuild action
         self.deploy_stage.add_action(codepipeline_actions.CodeBuildAction(
             input=self.source_artifact,
-            project=self.codebuild_deploy,
+            project=self.codebuild_deploy.project,
             action_name='Deploy_Changes'
         ))
-
+        
         
 app = core.App()
 
